9379a696995258233f59e75b580f4e8064677ba5
Zerosss <keqiangs@marshall.usc.edu>
2016-12-05 22:45:27 -0800

    Update overalltrend.R

--- overalltrend.R
+++ overalltrend.R
@@ -25,4 +25,22 @@ ggplot(Q1,aes(x=factor(yearmon),y=count,fill=RequestSource))+
   xlab('month')+
   ggtitle('Overall trends in calls / apps')
 
+
+Q2=data%>%
+  filter(RequestSource=='Web Form'|RequestSource=='Twitter')%>%
+  group_by(year(CreatedDate),month(CreatedDate),RequestSource)%>%
+  summarise(count=n())
+names(Q2)[1]='year'
+names(Q2)[2]='month'
+
+library(zoo)
+
+Q2$time <-  paste(Q2$year, Q2$month, sep="-")
+Q2$yearmon <- as.yearmon(paste(Q2$year, Q2$month), "%Y %m")
+
+ggplot(Q2,aes(x=factor(yearmon),y=count,fill=RequestSource))+
+  geom_bar(stat='identity',position='dodge')+
+  xlab('month')+
+  ggtitle('Web Forms/Twitter')
+
  
